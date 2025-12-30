ea0d623755f834f840db4f57b365203c888d9135
JiJi <jeromealmoguera@gmail.com>
2025-12-30 22:14:05 +0800

    Implement Tailwind and replaced css with Tailwind

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -71,38 +71,38 @@ export default function TodoList({ todos, onToggle, onDelete, onEdit }: TodoList
 	};
 
 	return (
-		<div className="list-container">
+		<div className="list-container ">
 			{todos.length === 0 ? (
 				<p>No Task Yet</p>
 			) : (
 				todos.map((todo) => (
-					<ul className="todo-list" key={todo.id}>
-						<li className="todo-item">
+					<ul className="todo-list " key={todo.id}>
+						<li className="group todo-item py-2.5 pl-7 rounded-2xl flex items-center mb-2 h-15 w-1/2 relative  bg-white shadow-sm">
 							{editTodos === todo.id ? (
-								<div className="edit">
+								<div className="edit flex items-center w-full">
 									<input
 										ref={editInputRef}
 										type="text"
 										value={editText}
 										onChange={(e) => setEditText(e.target.value)}
-										className="edit-input"
+										className="edit-input flex-1 bg-editInput h-10 outline-none rounded-full pl-5 mr-2.5 text-base"
 										onKeyDown={(e) => {
 											e.key === "Enter" && saveEdit();
 											e.key === "Escape" && cancelEdit();
 										}}
 									/>
 
-									<span onClick={saveEdit} className="material-symbols-outlined edit-save">
+									<span onClick={saveEdit} className="material-symbols-outlined google-icon bg-save text-white hover:outline-solid hover:outline-outline hover:outline-2 p-2.5 cursor-pointer rounded-full mr-1.5">
 										check
 									</span>
-									<span onClick={cancelEdit} className="material-symbols-outlined edit-cancel">
+									<span onClick={cancelEdit} className="material-symbols-outlined google-icon bg-cancel text-white hover:outline-solid hover:outline-outline hover:outline-2 p-2.5 cursor-pointer rounded-full mr-3.5">
 										close
 									</span>
 								</div>
 							) : (
 								<>
-									<div>
-										<input type="checkbox" checked={todo.completed} onChange={() => onToggle(todo.id)} className="todo-check" />
+									<div className="flex">
+										<input className="todo-check h-3.5 w-3.5 mr-3 " type="checkbox" checked={todo.completed} onChange={() => onToggle(todo.id)} />
 									</div>
 
 									<div
@@ -115,7 +115,7 @@ export default function TodoList({ todos, onToggle, onDelete, onEdit }: TodoList
 										{todo.text}
 									</div>
 
-									<div className="item-edit">
+									<div className="group-hover:visible item-edit ml-auto mr-5 flex items-center justify-center cursor-pointer invisible rounded-full text-menu hover:bg-menuH hover:text-menu">
 										<span onClick={() => openMenu(todo.id)} className="material-symbols-outlined ">
 											more_horiz
 										</span>
@@ -124,11 +124,11 @@ export default function TodoList({ todos, onToggle, onDelete, onEdit }: TodoList
 							)}
 
 							{openMenuId === todo.id && (
-								<div className="menu">
-									<span className="menu-edit" onClick={() => startEdit(todo.id)}>
+								<div className="menu text-xs bg-white shadow-sm box-border ml-2.5 p-1 rounded-xl h-full w-1/12 flex left-full flex-col absolute">
+									<span className="menu-edit flex items-center flex-1 rounded-lg m-0 py-0 pl-2 cursor-pointer hover:bg-editH " onClick={() => startEdit(todo.id)}>
 										Edit
 									</span>
-									<span className="menu-del" onClick={() => onDelete(todo.id)}>
+									<span className="menu-del flex items-center flex-1 rounded-lg m-0 py-0 pl-2 cursor-pointer hover:bg-editH " onClick={() => onDelete(todo.id)}>
 										Delete
 									</span>
 								</div>
